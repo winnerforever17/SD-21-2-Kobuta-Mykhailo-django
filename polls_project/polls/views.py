688e3305cad1a6b162604669c66fedf7751e2d7d
winnerforever17 <mykhailo.s.kobuta@ukd.edu.ua>
2024-03-20 10:25:43 +0200

    django.pr8

---
+++ polls_project/polls/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question})
