f729555d3ae79fc20544ebbc5f792b45cc9aaffc
winnerforever17 <mykhailo.s.kobuta@ukd.edu.ua>
2024-03-27 10:25:31 +0200

    django.pr9

--- polls_project/polls/urls.py
+++ polls_project/polls/urls.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
+    path('create/', views.create_question, name='create_question'),
+    path('<int:question_id>/update/', views.update_question, name='update_question'),
+    path('<int:question_id>/delete/', views.delete_question, name='delete_question'),
 ]
--- polls_project/polls/views.py
+++ polls_project/polls/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
+from .forms import QuestionForm
+
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    latest_questions = Question.objects.all()
+    context = {'latest_questions': latest_questions}
     return render(request, 'index.html', context)
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'detail.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def create_question(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('polls:index')
+    else:
+        form = QuestionForm()
+    return render(request, 'polls/create_question.html', {'form': form})
+
+
+def update_question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            form.save()
+            return redirect('polls:index')
+    else:
+        form = QuestionForm(instance=question)
+    return render(request, 'polls/update_question.html', {'form': form, 'question': question})
+
+
+def delete_question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == 'POST':
+        question.delete()
+        return redirect('polls:index')
+    return render(request, 'polls/delete_question.html', {'question': question})
